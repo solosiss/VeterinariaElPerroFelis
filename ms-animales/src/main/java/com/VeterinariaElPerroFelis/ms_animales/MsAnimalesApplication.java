53a0f1df5e84ea7c605d0e73685926a2d3b38e6c
solosiss <to.salasm@duocuc.cl>
2026-05-13 20:44:39 -0400

    inicializar proyecto veterinaria

---
+++ ms-animales/src/main/java/com/VeterinariaElPerroFelis/ms_animales/MsAnimalesApplication.java
@@ -0,0 +1,13 @@
+package com.VeterinariaElPerroFelis.ms_animales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAnimalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAnimalesApplication.class, args);
+	}
+
+}
---
+++ ms-animales/src/test/java/com/VeterinariaElPerroFelis/ms_animales/MsAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VeterinariaElPerroFelis.ms_animales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
